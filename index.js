ac52fb52207e598eab91ea60514c12057084f836
Davsav <davidbyu6@gmail.com>
2021-05-22 09:00:17 -0600

    added ability to add mulitpule interns

--- index.js
+++ index.js
@@ -69,11 +69,11 @@ const promptManager = () => {
 };
 
 const promptEngineer = engineerData => {
-console.log(`
-==========
-Add a New Eng
-=========
-`);
+// console.log(`
+// ==========
+// Add a New Eng
+// =========
+// `);
 
     if (!engineerData.teamMem) {
         engineerData.teamMem = [];
@@ -150,70 +150,92 @@ Add a New Eng
      });
 };
 
-const promptIntern = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: "What is the intern's name? (Required)",
-            validate: nameInput => {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log("Please enter the intern's name!");
-                    return false;
+const promptIntern = internData => {
+// console.log(`
+// ===========
+// and a new intern
+// =========
+// `);
+
+    if (!internData.teamMem) {
+        internData.teamMem = [];
+    }
+    return inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "What is the intern's name? (Required)",
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log("Please enter the intern's name!");
+                        return false;
+                    }
                 }
-            }
-        },
-        {
-            type: 'input',
-            name: 'empID',
-            message: "Enter the intern's employee ID.",
-            validate: githubInput => {
-                if (githubInput) {
-                    return true;
-                } else {
-                    console.log("Please enter the intern's Employee ID!");
-                    return false;
+            },
+            {
+                type: 'input',
+                name: 'empID',
+                message: "Enter the intern's employee ID.",
+                validate: githubInput => {
+                    if (githubInput) {
+                        return true;
+                    } else {
+                        console.log("Please enter the intern's Employee ID!");
+                        return false;
+                    }
                 }
-            }
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: "Enter the intern's email address.",
-            validate: githubInput => {
-                if (githubInput) {
-                    return true;
-                } else {
-                    console.log("Please enter the intern's email address!");
-                    return false;
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "Enter the intern's email address.",
+                validate: githubInput => {
+                    if (githubInput) {
+                        return true;
+                    } else {
+                        console.log("Please enter the intern's email address!");
+                        return false;
+                    }
                 }
-            }
-        },
-        {
-            type: 'input',
-            name: 'school',
-            message: "Enter the intern's school",
-            validate: githubInput => {
-                if (githubInput) {
-                    return true;
-                } else {
-                    console.log("Please enter the intern's school!");
-                    return false;
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: "Enter the intern's school",
+                validate: githubInput => {
+                    if (githubInput) {
+                        return true;
+                    } else {
+                        console.log("Please enter the intern's school!");
+                        return false;
+                    }
                 }
+            },
+            {
+                type: 'confirm',
+                name: 'confirmAddIntern',
+                message: 'Would you like to enter another Intern?',
+                default: false
             }
-        },
-  
-    ]);
+    
+    ])
+    .then(smData => {
+        internData.teamMem.push(smData);
+        if (smData.confirmAddIntern) {
+            return promptIntern(internData);
+        } else {
+            return internData;
+        }
+    });
 };
 
 promptManager()
     .then(promptEngineer)
+    .then(promptIntern)
     .then(engineerData => {
         console.log(engineerData);
     })
-
-    // .then(engineerAnswers => console.log(engineerAnswers))
-    // .then(promptIntern)
-    // .then(internAnswers => console.log(internAnswers));
+    .then(internAnswers => console.log(internAnswers));
